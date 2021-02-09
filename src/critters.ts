feac05fe5e4b61355aef6ad3c1ed0aa476e61b0a
Gus Porter <kiwiangus360@gmail.com>
2021-02-09 16:15:23 +1300

    Moved files

---
+++ src/critters.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import Critter from './types';
+import * as months from './months';
+import data from './critters.json';
+
+function getData(): Critter[] {
+  return data.critters;
+}
+
+function writeData(critters: Object[]): void {
+  const stringData = JSON.stringify({critters: critters});
+  fs.writeFile('./critters.json', stringData, (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('JSON data has been saved.');
+  });
+}
+
+function main() {
+  const critters = getData();
+
+  writeData(critters);
+}
+
+main();
