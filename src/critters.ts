5fc133352c6669a8e7ddba7ec29caeacb927427e
Gus Porter <kiwiangus360@gmail.com>
2021-02-09 17:08:27 +1300

    Added choices

--- src/critters.ts
+++ src/critters.ts
@@ -17,10 +17,47 @@ function writeData(critters: Critter[]): void {
   });
 }
 
+function getChoice(): number {
+  /**
+   * 1. update what has been caught
+   * 2. show what is needed to catch a particular month
+   * -> add * if it is not available next month
+   */
+
+  let result: number = -1;
+
+  while (result === -1) {
+    const prompt = require('prompt-sync')();
+
+    console.log('What would you like to do?');
+    console.log('1. Update what you have caught.');
+    console.log('2. See what critters are available to catch this month.');
+    console.log('0. Exit.');
+    const choice = prompt('Enter (0-2) ');
+
+    result = Number.parseInt(choice);
+    if (isNaN(result) || result < -1 || result > 2) {
+      result = -1;
+      console.log('Please enter a correct value.');
+    }
+  }
+
+  return result;
+}
+
+function updateCaughtCritters(critters: Critter[]): void {}
+
+function showCrittersByMonth(critters: Critter[]): void {}
+
 function main() {
   const critters = getData();
-
-  writeData(critters);
+  const result = getChoice();
+  if (result === 1) {
+    updateCaughtCritters(critters);
+  } else if (result === 2) {
+    showCrittersByMonth(critters);
+  }
+  // writeData(critters);
 }
 
 main();
