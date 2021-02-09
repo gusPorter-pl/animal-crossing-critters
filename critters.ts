81c57523f0a4708a3557afa54a43d75039fc9742
Gus Porter <kiwiangus360@gmail.com>
2021-02-09 15:18:30 +1300

    Moved functions to separate file

--- critters.ts
+++ critters.ts
@@ -1,40 +1,6 @@
 import fs from 'fs';
+import * as months from './months';
 import data from './critters.json';
-
-const expandMonths = (months: string): string[] => {
-  // converts hyphenated months out to every month
-  console.log(months);
-  const monthOrder = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  const splitMonths = months.split(",");
-  let returnMonths: string[] = [];
-  
-  for (let index = 0; index < splitMonths.length; index++) {
-    let splitMonth = splitMonths[index];
-    if (index > 0) {
-      splitMonth = splitMonth.substr(1);
-    }
-
-    const start = splitMonth.substring(0, 3);
-    const end = splitMonth.substring(4, 7);
-    const startNumber = monthOrder.indexOf(start);
-    const endNumber = monthOrder.indexOf(end) == -1 ? startNumber : monthOrder.indexOf(end);
-    
-    let i = startNumber;
-    do {
-      i = i == 12 ? 0 : i; // if i is 12, take it back to the start of the list
-      returnMonths.push(monthOrder[i]);
-      i++;
-    } while (i != endNumber + 1);
-  }
-  return returnMonths;
-}
-
-const condenseMonths = (months: string[]): string => {
-  // shortens months to hyphenated version
-  const firstMonth = months[0];
-  const lastMonth = months[months.length - 1];
-  return firstMonth + "-" + lastMonth;
-}
 
 function main() {
   // const stringData = JSON.stringify(data);
---
+++ months.ts
@@ -0,0 +1,34 @@
+export const expandMonths = (months: string): string[] => {
+  // converts hyphenated months out to every month
+  console.log(months);
+  const monthOrder = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const splitMonths = months.split(",");
+  let returnMonths: string[] = [];
+  
+  for (let index = 0; index < splitMonths.length; index++) {
+    let splitMonth = splitMonths[index];
+    if (index > 0) {
+      splitMonth = splitMonth.substr(1);
+    }
+
+    const start = splitMonth.substring(0, 3);
+    const end = splitMonth.substring(4, 7);
+    const startNumber = monthOrder.indexOf(start);
+    const endNumber = monthOrder.indexOf(end) == -1 ? startNumber : monthOrder.indexOf(end);
+    
+    let i = startNumber;
+    do {
+      i = i == 12 ? 0 : i; // if i is 12, take it back to the start of the list
+      returnMonths.push(monthOrder[i]);
+      i++;
+    } while (i != endNumber + 1);
+  }
+  return returnMonths;
+}
+
+export const condenseMonths = (months: string[]): string => {
+  // shortens months to hyphenated version
+  const firstMonth = months[0];
+  const lastMonth = months[months.length - 1];
+  return firstMonth + "-" + lastMonth;
+}
