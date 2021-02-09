7f5826e11c226ecc7b6de5f223789aa95e46cf39
Gus Porter <kiwiangus360@gmail.com>
2021-02-09 15:27:20 +1300

    Added format rules

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  arrowParens: 'always',
+  trailingComma: 'none'
+}
